7cf5db98b2cde210d6da5589c39b1a0af10fa373
shimengyao <1435831394@qq.com>
2019-09-03 21:25:10 +0800

    该app的详细信息

---
+++ ApplicationDetails/app/src/main/java/com/smy/applicationdetails/MainActivity.java
@@ -0,0 +1,31 @@
+package com.smy.applicationdetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.ApplicationDetails);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.fromParts("package", getPackageName(), null));
+                startActivity(intent);
+            }
+        });
+    }
+}
