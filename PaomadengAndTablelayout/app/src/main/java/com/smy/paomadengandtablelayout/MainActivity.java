10acd10224614c001ad9a5d878f376cdffb08e0a
shimengyao <1435831394@qq.com>
2019-10-09 21:39:31 +0800

    跑马灯

---
+++ PaomadengAndTablelayout/app/src/main/java/com/smy/paomadengandtablelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.smy.paomadengandtablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
